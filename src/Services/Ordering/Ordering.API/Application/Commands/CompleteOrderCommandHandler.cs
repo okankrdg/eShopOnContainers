49b514532231b5ad459bff26a70638aa554e0e19
Okan Can Karadağ <karadagokancan@gmail.com>
2023-06-10 02:16:13 +0300

    Added CompletedOrderCommandHandler and Model

---
+++ src/Services/Ordering/Ordering.API/Application/Commands/CompleteOrderCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿namespace Microsoft.eShopOnContainers.Services.Ordering.API.Application.Commands;
+
+public class CompleteOrderCommandHandler : IRequestHandler<CompleteOrderCommand, bool>
+{
+    public IOrderRepository _orderRepository { get; }
+    public CompleteOrderCommandHandler(IOrderRepository orderRepository)
+    {
+        _orderRepository = orderRepository;
+    }
+
+
+    public async Task<bool> Handle(CompleteOrderCommand request, CancellationToken cancellationToken)
+    {
+        var order = await _orderRepository.GetAsync(request.OrderNumber);
+        if (order == null)
+        {
+            return false;
+        }
+        order.SetCompletedStatus();
+        return await _orderRepository.UnitOfWork.SaveEntitiesAsync(cancellationToken);
+
+    }
+}
+public class CompleteOrderIdenfiedCommandHandler : IdentifiedCommandHandler<CompleteOrderCommand, bool>
+{
+    public CompleteOrderIdenfiedCommandHandler(
+        IMediator mediator,
+        IRequestManager requestManager,
+        ILogger<IdentifiedCommandHandler<CompleteOrderCommand, bool>> logger)
+        : base(mediator, requestManager, logger)
+    {
+    }
+
+    protected override bool CreateResultForDuplicateRequest()
+    {
+        return true; 
+    }
+}
