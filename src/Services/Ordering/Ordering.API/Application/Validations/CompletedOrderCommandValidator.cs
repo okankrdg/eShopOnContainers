a90da95bb72952aaa354f7c49b5f0cc6af293bc6
Okan Can Karadağ <karadagokancan@gmail.com>
2023-06-10 02:20:10 +0300

    Create CompletedOrderCommandValidator

---
+++ src/Services/Ordering/Ordering.API/Application/Validations/CompletedOrderCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.eShopOnContainers.Services.Ordering.API.Application.Validations;
+
+public class CompletedOrderCommandValidator : AbstractValidator<CompleteOrderCommand>
+{
+    public CompletedOrderCommandValidator(ILogger<CompletedOrderCommandValidator> logger)
+    {
+        RuleFor(order => order.OrderNumber).NotEmpty().WithMessage("No orderId found");
+
+        logger.LogTrace("INSTANCE CREATED - {ClassName}", GetType().Name);
+    }
+}
+
